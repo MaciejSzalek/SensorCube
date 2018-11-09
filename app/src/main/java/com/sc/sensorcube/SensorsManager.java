3b78b5a9cf8d810e1d2a8a271060f18e36fac3f5
MaciejSzalek <maciejszalek84@gmail.com>
2018-11-09 16:48:37 +0100

    Commit

---
+++ app/src/main/java/com/sc/sensorcube/SensorsManager.java
@@ -0,0 +1,16 @@
+package com.sc.sensorcube;
+
+import android.hardware.Sensor;
+
+/**
+ * Created by Maciej Szalek on 2018-10-09.
+ */
+
+public class SensorsManager {
+
+    private Sensor mSensor;
+
+    public SensorsManager(){}
+
+
+}
