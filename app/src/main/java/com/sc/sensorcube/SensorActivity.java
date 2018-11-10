571c0c025b15951bcc8430690c0490e874deae30
MaciejSzalek <maciejszalek84@gmail.com>
2018-11-10 13:44:12 +0100

    Commit

--- app/src/main/java/com/sc/sensorcube/SensorActivity.java
+++ app/src/main/java/com/sc/sensorcube/SensorActivity.java
@@ -2,6 +2,7 @@ package com.sc.sensorcube;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,6 +11,10 @@ public class SensorActivity extends AppCompatActivity {
     public Button startListenerButton;
     public Button stopListenerButton;
     public TextView sensorTextView;
+
+    private SensorsManager sensor;
+
+    private int rotation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,38 @@ public class SensorActivity extends AppCompatActivity {
         stopListenerButton = findViewById(R.id.sensor_stop_button);
         sensorTextView = findViewById(R.id.sensor_text);
 
+        sensor = new SensorsManager(this);
+
+        setupSensor();
+
+        startListenerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sensor.startListener();
+            }
+        });
+        stopListenerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sensor.stopListener();
+            }
+        });
+
+
+    }
+    private void setupSensor(){
+        sensor = new SensorsManager(this);
+        SensorsManager.SensorListener sensorListener = new SensorsManager.SensorListener() {
+            @Override
+            public void onNewParameters(double x, double y, double z) {
+                rotation = getWindowManager().getDefaultDisplay().getRotation();
+                sensor.setRotation(rotation);
+                sensorTextView.setText("Rotation: " + rotation
+                        + "\n X: " + x
+                        + "\n Y: " + y
+                        + "\n Z: " + z);
+            }
+        };
+        sensor.setListener(sensorListener);
     }
 }
